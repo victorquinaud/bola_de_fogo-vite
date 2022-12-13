750c1bde5666728f528baa74c60191215218e2f5
Victor Quinaud <quinaud.dev@gmail.com>
2022-12-13 20:56:36 -0300

    ViewProvider remove useState for testing

--- src/store/ViewProvider.tsx
+++ src/store/ViewProvider.tsx
@@ -1,10 +1,9 @@
-import { PropsWithChildren, createContext, useState } from "react";
+import { PropsWithChildren, createContext } from "react";
 import usePersistedState from './../hooks/usePersistedState';
 
 export const ViewContext = createContext<TViewResponse | null>(null);
 
 export const ViewProvider = ({ children }: PropsWithChildren) => {
-  // const [view, setView] = useState<TView>("start");
   const { state: view, setState: setView } = usePersistedState<TView>("VIEW", "start");
 
   return (
